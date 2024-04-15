947f52be674e247166645ab525b418de701a37d8
Isaac Wilkie <isaac.wilkie@gmail.com>
2024-04-15 14:08:06 -0600

    added init.lua

---
+++ init.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", "<leader>rr", ":!python %<CR>", { desc = "[R]un python code" })
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sys import exit
 _ = pygame.init()
 
 screen = pygame.display.set_mode((720,480))
+clock = pygame.Clock()
 
 while True:
     for event in pygame.event.get():
@@ -12,6 +13,4 @@ while True:
             pygame.quit()
             exit()
 
-
-
-    
+    _ = clock.tick(30)
