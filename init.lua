e381c2c42bbd7eb31cfe30bd8b2f11ad6fb60980
Isaac Wilkie <isaac.wilkie@gmail.com>
2024-04-25 11:41:16 -0600

    revised mux and init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
-vim.keymap.set("n", "<leader>rr", ":!python %<CR>", { desc = "[R]un python code" })
+-- set ctrl-shift-R to run the python script
+vim.keymap.set("n", "<C-R>", ":!python %<CR>", { desc = "[R]un python code" })
